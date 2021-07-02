a8b50dac88929892538643f30d5bb7cdd66d6150
yesthecarlos <carlosmendez86@gmail.com>
2021-07-02 14:14:23 -0700

    add beer image and hardcoded kegList

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
+import beerImage from "./../img/beer.png";
 
 function Header() {
   return (
+    <>
     <h1>Tap Room</h1>
+    <img src={beerImage} alt="A glass of beer" />
+    </>
   );
 }
 
--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,14 +1,42 @@
 import React from "react";
 import Keg from "./Keg";
 
+const masterKegList = [
+  {
+    name: 'Space Race',
+    brand: 'Hoodinville',
+    price: 5,
+    alcoholContent: 7,
+    pintsLeft: 124
+  },
+  {
+    name: 'Perseus',
+    brand: 'Ishtar Brews',
+    price: 7,
+    alcoholContent: 9,
+    pintsLeft: 56
+  },
+  {
+    name: 'Hunter',
+    brand: 'TopBrews',
+    price: 8,
+    alcoholContent: 6,
+    pintsLeft: 79
+  }
+];
+
 function KegList(){
   return (
-    <Keg
-     name="Space Race"
-     brand="Hoodinville"
-     price="5"
-     alcoholContent="7"
-     pintsLeft="124" />
+    <React.Fragment>
+      <hr />
+      {masterKegList.map((keg, index) =>
+      <Keg name={keg.name}
+        brand={keg.brand}
+        price={keg.price}
+        alcoholContent={keg.alcoholContent}
+        pintsLeft={keg.pintsLeft}/>
+      )}
+    </React.Fragment>
   );
 }
 
