de46ba7553f82c5272044a4917d86093be1b6730
yesthecarlos <carlosmendez86@gmail.com>
2021-07-05 18:46:42 -0700

    add button and event handler

---
+++ src/components/KegControl.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import NewKegForm from './NewKegForm';
+import KegList from './KegList';
+
+class KegControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevSate => ({
+      formVisibleOnPage: !prevSate.formVisibleOnPage
+    }));
+  }
+
+  render() {
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewKegForm />;
+      buttonText = "Return to Keg List";
+    } else {
+      currentlyVisibleState = <KegList />
+      buttonText = "Add Keg";
+    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default KegControl;
